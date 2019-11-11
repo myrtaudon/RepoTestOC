909b2f086ab74ad5187a86e4e08319e239dafb22
mtaud <mtaud@DESKTOP-RMJ2H62.home>
2019-11-11 10:01:45 +0100

    Evol2: Variabilisation du message hello

--- ProjectOC/src/main/java/org/example/demo/ProjectOC/App.java
+++ ProjectOC/src/main/java/org/example/demo/ProjectOC/App.java
@@ -1,5 +1,7 @@
 package org.example.demo.ProjectOC;
 
+import org.example.demo.model.Personne;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Personne p = new Personne("Myriam", (short)40);
+        System.out.println( "Hello "+p.getName()+" !" );
     }
 }
